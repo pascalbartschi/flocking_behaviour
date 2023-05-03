fc0a3101d42324c055852d6e246a81cd8c5a5399
pascalbartschi <103056293+pascalbartschi@users.noreply.github.com>
2023-05-03 10:23:04 +0200

    possible research questions and ideas

---
+++ research_questions.md
@@ -0,0 +1,17 @@
+### Basic Simulation
+
+* How does the cpull_f affect the behaviour of the flocking?
+
+* How do diiferent initialization patterns of the agents affect the flock (random vs. clustered)?
+
+* How does the size of the flock affect the behaviour of the flocking behaviour?
+
+### Needs further development
+
+* Center of mass is now one for all. How would it look like if there were plenty of CoM for given neighbours in radius
+
+* Leadership agents. Implement leaders that have more influence on behaviour
+
+* External stimuli: Wind force/noise, predator, a food supply
+
+* Optimizaion Problem: e.g maximizing the speed of the flock while minimizing the distance between agents.
---
+++ research_questions_basic.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May  2 16:33:53 2023
+
+@author: pascal
+"""
+import flocking_behaviour_basic
+
+## Basic Simulation
+
+# How does the cpull_f affect the behaviour of the flocking
+
+# How do diiferent initialization patterns of the agents affect the flock (random vs. clustered)?
+
+# How does the size of the flock affect the behaviour of the flocking behaviour
+
+## Needs further development:
+    
+# center of mass is now one for all. How would it look like if there where plenty of com for given neighbours in 
+# radius
+
+# Leadership agents. Implement leaders that have more influence on behaviour
+
+# External stimuli: Wind force/noise, predator, a food supply
+
+# 
+
+
