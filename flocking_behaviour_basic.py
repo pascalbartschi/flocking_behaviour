8f589934fc20dc98f1dfa53425be50eadc3b8a9a
pascalbartschi <103056293+pascalbartschi@users.noreply.github.com>
2023-05-03 10:25:35 +0200

    developed code for broader usability
    - inline plotting and storing possible
    - easy changing of update and initializing functions
    - easy switch of dimensions

--- flocking_behaviour_basic.py
+++ flocking_behaviour_basic.py
@@ -3,27 +3,40 @@ import matplotlib.pyplot as plt
 # from mpl_toolkits.mplot3d import Axes3D
 
 
-def initialize(n = 100, d = 2, init_coord = (-1, 1)):
+def initialize_random(param):
     '''
     This function initializes the agents, as well as the plotting window
 
     Parameters
     ----------
-    n : int, optional
-        Number of Agents. The default is 100.
-    d : int, optional
-        Dimensions of Movement. The default is 2.
-    init_coord : tuple, optional
-        Range where Agents are intialized. The default is (-1, 1).
+    param : dict
+        Holds the necessary parameters.
+
+    Raises
+    ------
+    ValueError
+        Dimension is not supplied or invalid.
 
     Returns
     -------
-    agent_old, agents_now, , agent_temp, figure, ax
+    agent_old : np.ndarray
+        Zeros.
+    agent_now : np.ndarray
+        Initial positions of agents.
+    fig : figure object
+        Figure to plot in.
+    ax : artist object
+        Artist axis to plot in.
 
     '''
+    n = param["n"]
+    d = param["d"]
+    low, high = param["init_coord"]
+    lower_lim, upper_lim = param["ax_lim"]
+    
 
     # initialize n agents in d dimensions
-    low, high = init_coord
+    
     
     agent_now = np.random.uniform(low=low, high=high, size=(n, d))
     agent_old = np.zeros_like(agent_now)
@@ -33,20 +46,20 @@ def initialize(n = 100, d = 2, init_coord = (-1, 1)):
         # some fig to plot
         fig = plt.figure()
         ax = fig.add_subplot(1, 1, 1)
-        ax.set_xlim(-50, 50)
-        ax.set_ylim(-50, 50)
+        ax.set_xlim(lower_lim, upper_lim)
+        ax.set_ylim(lower_lim, upper_lim)
     elif d == 3: 
         fig = plt.figure()
         ax = fig.add_subplot(1, 1, 1, projection = "3d")
-        ax.set_xlim(-50, 50)
-        ax.set_ylim(-50, 50)
-        ax.set_zlim(-50, 50)
+        ax.set_xlim(lower_lim, upper_lim)
+        ax.set_ylim(lower_lim, upper_lim)
+        ax.set_zlim(lower_lim, upper_lim)
     else:
         raise ValueError("Incorrect number of dimensions. Choose d=2 or d=3.")
 
     return agent_old, agent_now, fig, ax
 
-def plot_2D(agent_now, ax):
+def inline_plot_2D(agent_now, ax, param):
     '''
     Plots in 2D after clearing axis object
 
@@ -60,17 +73,18 @@ def plot_2D(agent_now, ax):
     ax : axis object
 
     '''
+    lower_lim, upper_lim = param["ax_lim"]
     # plot 2D
     ax.clear()
     ax.scatter(agent_now[:, 0], agent_now[:, 1])
-    ax.set_xlim(-50, 50)
-    ax.set_ylim(-50, 50)
+    ax.set_xlim(lower_lim, upper_lim)
+    ax.set_ylim(lower_lim, upper_lim)
     plt.pause(0.01)
     
     return ax
     
 
-def plot_3D(agent_now, ax):
+def inline_plot_3D(agent_now, ax, param):
     '''
     Plots in 3D after clearing axis object
 
@@ -84,11 +98,12 @@ def plot_3D(agent_now, ax):
     ax : axis object
 
     '''
+    lower_lim, upper_lim = param["ax_lim"]
     ax.clear()
     ax.scatter(agent_now[:, 0], agent_now[:, 1], agent_now[:, 2])
-    ax.set_xlim(-50, 50)
-    ax.set_ylim(-50, 50)
-    ax.set_zlim(-50, 50)
+    ax.set_xlim(lower_lim, upper_lim)
+    ax.set_ylim(lower_lim, upper_lim)
+    ax.set_zlim(lower_lim, upper_lim)
     plt.pause(0.01)
     
     return ax
@@ -97,7 +112,7 @@ def euclidian_dist(array):
 
     return np.sqrt(np.sum(array**2, axis = 1))
 
-def update(agent_now, agent_old, d = 3, cpull_f = 1.5):
+def update(agent_now, agent_old, param):
     '''
     Update of the postion of the agents, with the assumption that their acceleretion is computed from
     their pull towards the center of mass and the delta between their new and old position
@@ -107,7 +122,7 @@ def update(agent_now, agent_old, d = 3, cpull_f = 1.5):
         DESCRIPTION.
     agent_old : array (n, d)
         DESCRIPTION.
-    cpull_f : int, optional
+    center_pull : int, optional
         DESCRIPTION. The default is 1.5.
 
     Returns
@@ -115,6 +130,9 @@ def update(agent_now, agent_old, d = 3, cpull_f = 1.5):
     agent_temp : updated positon of agents
 
     '''
+    d = param["d"]
+    center_pull = param["center_pull"]
+    
     agent_temp = np.zeros_like(agent_now)
     # calculate center of mass
     C = np.mean(agent_now, axis=0)
@@ -122,48 +140,100 @@ def update(agent_now, agent_old, d = 3, cpull_f = 1.5):
     # update the agent position according to acceleration to center
     for j in range(d):
         agent_temp[:, j] = 2 * agent_now[:, j] - agent_old[:, j] + \
-            cpull_f * (C[j] - agent_now[:, j]) / euclidian_dist((C - agent_now))
+            center_pull * (C[j] - agent_now[:, j]) / euclidian_dist((C - agent_now))
     
     return agent_temp
     
 
-def simulate_flocking_2D(n = 100, init_coord = (-1, 1), steps = 100, cpull_f = 1.5): 
+def simulate_flocking(initialize_func = initialize_random,
+                      update_func = update,
+                      inline_plotting = True,
+                      d = 2,
+                      param = {"n" : 100,
+                               "init_coord":(-1, 1),
+                               "ax_lim": (-50, 50),
+                               "steps": 100,
+                               "center_pull": 1.5}): 
+    '''
+
+    Parameters
+    ----------
+    n : int , optional
+        number of agents. The default is 100.
+    init_coord : tuple, optional
+        Range of initialization of agents. The default is (-1, 1).
+    steps : int, optional
+        number of simulation steps. The default is 100.
+    center_pull : float, optional
+        pull factor towards center of mass. The default is 1.5.
+
+    Returns
+    -------
+    None.
+
+    '''
+    steps = param["steps"]
+    n = param["n"]
+    param["d"] = d
     
-    d = 2
+    agent_old, agent_now, fig, ax = initialize_func(param)
     
-    agent_old, agent_now, fig, ax = initialize(d = d)
-    
-    # simulate
-    for i in range(steps):
-    
-    
-        agent_temp= update(agent_now, agent_old, d, cpull_f)
-        # store updated and this position for next acceleration
-        agent_old = agent_now.copy()
-        agent_now = agent_temp.copy()
+    # inline plotting to explore
+    if inline_plotting:
+        if d == 2: 
+            inline_plotting_func = inline_plot_2D
+        elif d == 3: 
+            inline_plotting_func = inline_plot_3D
+        else:
+            raise ValueError("Please simulate in 2 or 3 dimensions to plot inline!")
         
-        # plot
-        plot_2D(agent_now, ax)
+        # simulate
+        for i in range(steps):
         
-def simulate_flocking_3D(n = 100, init_coord = (-1, 1), steps = 100, cpull_f = 1.5): 
-    
-    d = 3
-    
-    agent_old, agent_now, fig, ax = initialize(d = d)
-    
-    # simulate
-    for i in range(steps):
-    
-    
-        agent_temp= update(agent_now, agent_old, d, cpull_f)
-        # store updated and this position for next acceleration
-        agent_old = agent_now.copy()
-        agent_now = agent_temp.copy()
         
-        # plot
-        plot_3D(agent_now, ax)
+            agent_temp = update_func(agent_now, agent_old, param)
+            # store updated and this position for next acceleration
+            agent_old = agent_now.copy()
+            agent_now = agent_temp.copy()
+            
+            # plot
+            inline_plotting_func(agent_now, ax, param)
+        
+        # close the plotting window
+        plt.close()
+    
+    # return animation for saving
+    else:
+        plt.close()
+        # storage
+        positions = np.zeros((steps+1, n, d))
+        positions[0, :, :] = agent_now.copy()
+        
+        for i in range(steps):
+        
+        
+            agent_temp = update_func(agent_now, agent_old, param)
+            # store updated and this position for next acceleration
+            agent_old = agent_now.copy()
+            agent_now = agent_temp.copy()
+            
+            # insert in storage
+            positions[i+1, :, :] = agent_now.copy()
+            
+        return positions
+            
         
 
-simulate_flocking_2D()
-simulate_flocking_2D()
+        
+
+        
+if __name__ == "__main__":
+    # simulate in 2D
+    simulate_flocking(d = 2)
+    # simulate in 3D
+    simulate_flocking(d = 3)
+    # store a simulation
+    pos = simulate_flocking(d = 2, inline_plotting = False)
+
+
 
